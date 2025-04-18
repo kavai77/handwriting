32bdafaf55ffce279b71165e59963c03c07db503
Csaba Kavai <csaba.kavai.osv@fedex.com>
2025-04-18 13:20:38 +0200

    using /usr/local/secrets

--- src/main/java/com/himadri/handwriting/App.java
+++ src/main/java/com/himadri/handwriting/App.java
@@ -9,7 +9,7 @@ import static com.himadri.handwriting.App.SECRET_STORE;
 @SpringBootApplication
 @PropertySource("file://" + SECRET_STORE + "secrets.yml")
 public class App {
-    public static final String SECRET_STORE = "/usr/share/secrets/handwriting-secrets/";
+    public static final String SECRET_STORE = "/usr/local/secrets/handwriting-secrets/";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
